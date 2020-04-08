e7fd0e9007225256ae4106a0c6edebe13340c59d
Hettie McConnell <hettie.mcconnell@outlook.com>
2020-04-08 16:30:31 +0100

    Adding testing for create new user.
    Co-authored-by: HannahGooding <hannah.gooding@yahoo.com>

--- source/database/connection.js
+++ source/database/connection.js
@@ -4,9 +4,9 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const db = new pg.Pool({
-    connectionString: process.env.DATABASE_URL,
-    // if we have a database URL (e.g. from Heroku we'll use that)
-    // otherwise it'll default to your local .env variables
+  // connectionString: process.env.DATABASE_URL,
+  // if we have a database URL (e.g. from Heroku we'll use that)
+  // otherwise it'll default to your local .env variables
 });
 
 module.exports = db;
--- source/model.js
+++ source/model.js
@@ -6,24 +6,37 @@ const db = require("./database/connection");
 // }
 
 // THE BELOW DOES PROTECT AGAINST SQL INJECTION (HOPEFULLY)
-function getTools(filter){
-    return db.query("SELECT * FROM user_input WHERE category LIKE ($1)", [`${filter}`]).then(result => result.rows);;
+function getTools(filter) {
+  return db
+    .query("SELECT * FROM user_input WHERE category LIKE ($1)", [`${filter}`])
+    .then((result) => result.rows);
 }
 
-function createTool(userEntry){
-    const values = [userEntry.category, userEntry.tool_name, userEntry.tool_description, userEntry.tool_link, userEntry.added_by, userEntry.love];
-    return db.query(
-        "INSERT INTO user_input(category, tool_name, tool_description, tool_link, added_by, love) VALUES($1, $2, $3, $4, $5, $6)",
-        values
-    );
+function createTool(userEntry) {
+  const values = [
+    userEntry.category,
+    userEntry.tool_name,
+    userEntry.tool_description,
+    userEntry.tool_link,
+    userEntry.added_by,
+    userEntry.love,
+  ];
+  return db.query(
+    "INSERT INTO user_input(category, tool_name, tool_description, tool_link, added_by, love) VALUES($1, $2, $3, $4, $5, $6)",
+    values
+  );
 }
 
-function addLove(id){
-    return db.query(`
+function addLove(id) {
+  return db.query(`
     UPDATE user_input
     SET love = love + 1
     WHERE id = ${id}
     `);
 }
 
-module.exports = { getTools, createTool, addLove };
+function createNewUser() {}
+
+function getUsers() {}
+
+module.exports = { getTools, createTool, addLove, createNewUser, getUsers };
